78629bb8ab5ecc49f25a92d8e7ad781ee46d03ac
Freya-tian <58150499+Freya-tian@users.noreply.github.com>
2020-07-06 18:35:49 +0300

    Update PasswordController.java

--- server/src/main/java/com/freya/controller/PasswordController.java
+++ server/src/main/java/com/freya/controller/PasswordController.java
@@ -4,12 +4,13 @@ package com.freya.controller;
 
 import com.freya.entity.User;
 
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.springframework.web.bind.annotation.*;
 
 
 import java.util.LinkedHashMap;
 import java.util.Map;
-
+import java.util.UUID;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -17,18 +18,36 @@ public class PasswordController {
 
     public Map<String, String> map = new LinkedHashMap<String, String>();
 
-    @RequestMapping(value = "log", method = RequestMethod.POST)
 
+    public static String[] chars = new String[] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+
+    @RequestMapping(value = "log", method = RequestMethod.POST)
     public String ToPicture(@RequestBody String string) {
-       String string1 = StringEscapeUtils.unescapeJava(string);
+        String string1 = StringEscapeUtils.unescapeJava(string);
         String[] part = string1.split("\"");
-        String url = part[4];
-        System.out.println(url);
+
+        String file = part[4];//
+
+        String url = "http://localhost:8090/static/"+file;
+        System.out.println(file);
         String ret = null;
-        String passward = "3457844";
+
+        StringBuffer shortBuffer = new StringBuffer();
+        String uuid = UUID.randomUUID().toString().replace("-", "");
+        String passward ;
+        for (int i = 0; i < 8; i++) {
+            String str = uuid.substring(i * 4, i * 4 + 4);
+            int x = Integer.parseInt(str, 16);
+            shortBuffer.append(chars[x % 0x3E]);
+
+        }
+
+       passward=shortBuffer.toString();
+
+
         map.put(url, passward);
         ret = url+"\n"+map.get(url);
-
+        System.out.println(map.get(url));
         return ret;
     }
 
@@ -37,7 +56,7 @@ public class PasswordController {
     public String login(@RequestBody User user) {
         String password = user.getPassword();
         System.out.println(password);
-        int num = 1;
+
 
         for (String key : map.keySet()) {
 

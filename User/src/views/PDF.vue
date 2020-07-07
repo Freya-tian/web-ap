0b30140c5769d3243da314ddb845d94bdad0d394
Freya-tian <58150499+Freya-tian@users.noreply.github.com>
2020-07-07 21:50:59 +0300

    Update PDF.vue

--- User/src/views/PDF.vue
+++ User/src/views/PDF.vue
@@ -1,36 +1,29 @@
 <template>
-  <div class="pdf" v-show="fileType == 'pdf'">
-    <h3 >{{times}}</h3>
-    <pdf :src="src" > // 加载事件
+  <div >
+{{this.times}}
+    <img :src="this.src" alt=""/>
 
-    </pdf>
 <!--    <button type="button" class="btn btn-primary"v-on:click="gettim/*;setcookies('localhost:8081/ToPicture')*/">Look</button>-->
   </div>
 </template>
 
 <script>
   import pdf from 'vue-pdf'
-
-
-
   export default {
-
-    components: {
-
-      pdf,
-
-    },
+    // components: {
+    //   pdf,
+    // },
     data() {
       return {
         fileType: 'pdf',
-        src: '/static/pic.pdf',
-        times:'',
+        src:localStorage.getItem('APP'),
 
+        times:'',
       }
     },
-    mounted() {
+mounted() {
       this.gettim()
-    },
+},
 
     methods:{
       gettim(){
